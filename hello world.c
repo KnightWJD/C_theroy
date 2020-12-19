736da95714e651b493833b39a2e9444e2e6afdef
knightWJD <2630225285@qq.com>
2020-12-19 23:08:23 +0800

    first commit

---
+++ hello world.c
@@ -0,0 +1,12 @@
+//包含一个叫stdio.h的文件
+//stdio --  standard input output
+
+#include <stdio.h>
+
+//int 是整型的意思
+//mian 前面的 int 表示mian函数调用后返回一个整型值
+int main()//主函数-程序的入口
+{
+    printf("hello world\n");
+    return 0;
+}
